b516f4f05f6c7c7c7dcbfb10c5b2726690fbceb9
josellagosta <136128782+josellagosta@users.noreply.github.com>
2026-06-10 22:08:16 +0200

    feat: crear listado inicial de materiales

--- inventario/views.py
+++ inventario/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Material
 
-# Create your views here.
+
+def lista_materiales(request):
+    materiales = Material.objects.all()
+    return render(request, "inventario/lista_materiales.html", {
+        "materiales": materiales
+    })
